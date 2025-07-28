a3322470b7aa8f15f3aae7afa0bbe10aa4325e93
Naveen-67 <naveenchinnala123@gmail.com>
2025-07-28 10:03:01 +0530

    login page done and functions created for registration page

--- index.js
+++ index.js
@@ -3,17 +3,72 @@ import register from "./register.js"
 
 const root=document.getElementById('root')
 const allAnchors=document.querySelectorAll('a')
+
 const router={
-     "/login":login,
-     "/register":register
+    "/login":login,
+    "/register":register
 }
-function handleClick(e){
-     e.preventDefault()
-     // console.log(e.target/pathname)
+
+function handelClick(e){
+  e.preventDefault()
+//   console.log(e.target.pathname);
 let path=e.target.pathname
-     history.pushState(null,"",`${path}`)
-     root.innerHTML=router[path]()
-     }
-allAnchors.forEach((anchor) => {
-     anchor.addEventListener('click',handleClick)
-});
+  history.pushState(null,"",`${path}`)
+root.innerHTML=router[path]()
+}
+allAnchors.forEach((anchor)=>{
+    anchor.addEventListener("click",handelClick)
+})
+
+window.addEventListener('popstate',(e)=>{
+  // console.log(location);
+  let path=location.pathname
+if(path=="/index.html"){
+root.innerHTML=""
+}else{
+  root.innerHTML=router[path]()
+}
+  
+})
+
+
+
+const state={
+  setState(name,value){
+    this[name]=value
+  }
+}
+const form=document.querySelector('form')
+const inputs=document.querySelectorAll('input')
+const textArea=document.querySelector('textarea')
+
+
+function handelChnage(e){
+let {name,value,files}=e.target
+if(name!="re-password"){
+  if(name=="image"){
+value=files[0]
+const reader=new FileReader()
+reader.onload=function(){
+  form.style.backgroundImage=`url(${reader.result})`
+}
+reader.readAsDataURL(value)
+state.setState(name,value)
+  }else{
+  state.setState(name,value)
+
+  }
+}
+}
+
+function handelSubmit(e){
+e.preventDefault()
+console.log(state);
+
+}
+
+form.addEventListener('submit',handelSubmit)
+inputs.forEach(input=>{
+  input.addEventListener('change',handelChnage)
+})
+textArea.addEventListener('change',handelChnage)
